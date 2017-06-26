f571bb5a1d7efc93c67ef2b30e88f1c9e188406e
victor <victor.bucutea@gmail.com>
2017-06-27 01:22:37 +0300

    step1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import {TableComponent} from "./table.component";
-import {EmployeeService} from "./employee.service";
 import {FormsModule} from "@angular/forms";
-import {RouterModule, Routes} from "@angular/router";
-import {FormComponent} from "./form.component";
 
 
-const routes: Routes = [
+/*const routes: Routes = [
   {
     path: 'edit/:id',
     component: FormComponent
   }
-];
+];*/
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, TableComponent, FormComponent],
-  bootstrap:    [ AppComponent ],
-  providers:    [ EmployeeService ]
+  imports:      [ BrowserModule, FormsModule, /*RouterModule.forRoot(routes) */],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
